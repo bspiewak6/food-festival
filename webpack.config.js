ddfa8818b80c927ff39a9fede1cb23e8bab8aaea
Spiewak <bspiewak6@gmail.com>
2020-12-14 16:57:48 -0500

    figuring out jquery error bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const webpack = require('webpack');
-require('bootstrap');
+const path = require("path");
+const webpack = require("webpack");
+require("bootstrap");
 
 module.exports = {
-    entry: './assets/js/script.js', // webpack looks to start building module
+    entry: "./assets/js/script.js", // webpack looks to start building module
     output: { 
-        path: path.resolve(__dirname, 'dist'), 
-        filename: 'main.bundle.js' // location of this file is in the dist folder
+        path: path.resolve(__dirname, "dist"), 
+        filename: "main.bundle.js" // location of this file is in the dist folder
     },
     plugins:[
         new webpack.ProvidePlugin({
@@ -14,7 +14,7 @@ module.exports = {
           jQuery: "jquery"
         }),
     ],
-    mode: 'development' // re-loading and de-bugging features
+    mode: "development" // re-loading and de-bugging features
 };
 
 
