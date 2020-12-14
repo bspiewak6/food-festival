b044461abd0530cb60573d83f1909ed5829ece6f
Spiewak <bspiewak6@gmail.com>
2020-12-14 13:42:05 -0500

    basic webpack config set up, getting bug npm run build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+require('bootstrap');
+
+module.exports = {
+    entry: './assets/js/script.js', // webpack looks to start building module
+    output: { 
+        path: path.resolve(__dirname, 'dist'), 
+        filename: 'main.bundle.js' // location of this file is in the dist folder
+    },
+    plugins:[
+        new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery"
+        }),
+    ],
+    mode: 'development' // re-loading and de-bugging features
+};
+
+
+
+
