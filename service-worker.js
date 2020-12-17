a933c775b9508eb97b9a6c1d31585fad85a43307
Spiewak <bspiewak6@gmail.com>
2020-12-16 20:57:40 -0500

    starting to create manifest.json

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
+// global variables
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 // all files in root directory except for images
 const FILES_TO_CACHE = [
     "./index.html",
@@ -13,10 +18,25 @@ const FILES_TO_CACHE = [
     "./dist/schedule.bundle.js"
 ];
 
-// global variables
-const APP_PREFIX = 'FoodFest-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+// event listener to intercept fetch requests when user is offline
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else { // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // you can omit if/else for console.log & put one line below like this 
+            // return request || fetch(e.request)
+        })
+    )
+})
+
   
 // event listener to install service worker
 self.addEventListener('install', function (e) {
@@ -26,9 +46,9 @@ self.addEventListener('install', function (e) {
         return cache.addAll(FILES_TO_CACHE)
       })
     )
-});
+})
 
-// event listener to activate service worker
+// event listener to activate service worker AND DELETE outdated caches
 self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
@@ -48,21 +68,3 @@ self.addEventListener('activate', function (e) {
     );
 });
   
-// event listener to intercept fetch requests when user is offline
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else { // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-
-            // you can omit if/else for console.log & put one line below like this 
-            // return request || fetch(e.request)
-        })
-    )
-})
