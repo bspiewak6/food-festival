c8b695b598d28f6d2ecc49a05c3ac3ed44df9523
Spiewak <bspiewak6@gmail.com>
2020-12-16 20:21:59 -0500

    installed and activated service worker, added fetch to grab user data when offline

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+// all files in root directory except for images
 const FILES_TO_CACHE = [
     "./index.html",
     "./events.html",
@@ -12,10 +13,12 @@ const FILES_TO_CACHE = [
     "./dist/schedule.bundle.js"
 ];
 
+// global variables
 const APP_PREFIX = 'FoodFest-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
   
+// event listener to install service worker
 self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
@@ -23,5 +26,43 @@ self.addEventListener('install', function (e) {
         return cache.addAll(FILES_TO_CACHE)
       })
     )
-})
+});
+
+// event listener to activate service worker
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache: ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            })
+          );
+       })
+    );
+});
   
+// event listener to intercept fetch requests when user is offline
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else { // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // you can omit if/else for console.log & put one line below like this 
+            // return request || fetch(e.request)
+        })
+    )
+})
